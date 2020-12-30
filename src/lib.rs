9a1ae8e635d797a6288381a36da5af3305d5251e
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-12-30 07:42:42 +0100

    :up: Bumped to v0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub mod schema;
+pub mod worker;
+
+pub fn mut_to_vector<T>(data: *mut T, size: isize) -> Vec<T> {
+    if data.is_null() {
+        Vec::new()
+    } else {
+        let mut datas = Vec::new();
+        for index in 0..size {
+            let data = unsafe {
+                let data_ptr = data.offset(index);
+                Box::from_raw(data_ptr)
+            };
+            datas.push(*data);
+        }
+        datas
+    }
+}
+
+pub(crate) fn const_to_vector<T: Clone>(data: *const T, size: isize) -> Vec<T> {
+    if data.is_null() {
+        Vec::new()
+    } else {
+        let mut datas = Vec::new();
+        for index in 0..size {
+            let data = unsafe {
+                let data_ptr = data.offset(index);
+                (*data_ptr).clone()
+            };
+            datas.push(data);
+        }
+        datas
     }
 }
